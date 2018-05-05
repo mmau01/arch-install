b1706dcf072e57e117af5738a7f9bb8e95b1609d
Mike <35034778+mmau01@users.noreply.github.com>
2018-05-05 18:04:10 +1000

    Update post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -14,26 +14,15 @@ echo "blacklist ideapad_laptop" >> /etc/modprobe.d/blacklist.conf
 echo "blacklist wacom" >> /etc/modprobe.d/blacklist.conf
 
 # Set your mkinitcpio encrypt/lvm2 hooks and rebuild.
-sed -i 's/^MODULES=.*/MODULES=(ext4 i915)/' /etc/mkinitcpio.conf
-sed -i 's/^HOOKS=.*/HOOKS=(base udev keyboard autodetect modconf block encrypt lvm2 resume filesystems fsck)/' /etc/mkinitcpio.conf
-sed -i 's/^FILES=.*/FILES=(\/crypto_keyfile.bin)/' /etc/mkinitcpio.conf
-#mkinitcpio -p linux
-
-# Add a keyfile to decrypt and mount the boot volume during startup.
-dd bs=512 count=4 if=/dev/urandom of=/crypto_keyfile.bin
-chmod 000 /crypto_keyfile.bin
-chmod 600 /boot/initramfs-linux*
-cryptsetup luksAddKey /dev/nvme0n1p2 /crypto_keyfile.bin
-echo "cryptboot /dev/nvme0n1p2 /crypto_keyfile.bin luks" >> /etc/crypttab
+sed -i 's/^HOOKS=.*/HOOKS=(base udev autodetect modconf block keymap encrypt lvm2 resume filesystems keyboard fsck)/' /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
-# Configure GRUB.
+# Install and configure GRUB.
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux --recheck
+#sed -i 's/^#GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
 echo 'GRUB_FORCE_HIDDEN_MENU="true"' >> /etc/default/grub
 wget https://gist.githubusercontent.com/anonymous/8eb2019db2e278ba99be/raw/257f15100fd46aeeb8e33a7629b209d0a14b9975/gistfile1.sh -O /etc/grub.d/31_hold_shift
 chmod a+x /etc/grub.d/31_hold_shift
-sed -i 's/^#GRUB_ENABLE_CRYPTODISK=.*/GRUB_ENABLE_CRYPTODISK=y/' /etc/default/grub
 ROOTUUID=$(blkid /dev/nvme0n1p3 | awk '{print $2}' | cut -d '"' -f2)
-sed -i "s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$ROOTUUID:lvm:allow-discards root=\/dev\/mapper\/arch-root resume=\/dev\/mapper\/arch-swap\"/" /etc/default/grub
+sed -i "s/^GRUB_CMDLINE_LINUX=.*/GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=$ROOTUUID:lvm root=\/dev\/mapper\/arch-root resume=\/dev\/mapper\/arch-swap\"/" /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="grub" --recheck
-chmod -R g-rwx,o-rwx /boot
